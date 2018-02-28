4f0a3c7ae92a72f375a12e36560b5153645f04b9
Samuel Lindsey <Samuel@SAMUEL-PC>
2018-02-28 18:18:37 -0500

    int commit added bd for Books

--- BookList/Migrations/Configuration.cs
+++ BookList/Migrations/Configuration.cs
@@ -25,7 +25,8 @@ namespace BookList.Migrations
                   new Books { BookId = 1, Title = "Wind", Author = "Carol Thompson" },
                   new Books { BookId = 2, Title = "Grumpy Bird", Author = "Jeremy Tankard" },
                   new Books { BookId = 3, Title = "How Hippo Says Hello", Author = "Abigail Samoun" },
-                  new Books { BookId = 4, Title ="GEM", Author = "Holly Hobbie" }
+                  new Books { BookId = 4, Title ="GEM", Author = "Holly Hobbie" },
+                  new Books { BookId = 5, Title = "Happy", Author = "MeeToo"}
 
                 );
             
